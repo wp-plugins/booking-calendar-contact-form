9136bec89720a4115cb64a68ddf6e54bd19ff882
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 22:07:35 +0000

    Update to make the IPN work over SSL connections.
    git-svn-id: https://plugins.svn.wordpress.org/booking-calendar-contact-form/trunk@1102106 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_bccf.php
+++ dex_bccf.php
@@ -1375,6 +1375,8 @@ function cp_bccf_get_FULL_site_url($admin = false)
     $pos = strpos($url, "://");
     if ($pos === false)
         $url = 'http://'.$_SERVER["HTTP_HOST"].$url;
+    if (!empty($_SERVER['HTTPS']))     
+        $url = str_replace("http://","https://",$str);        
     return $url;
 }
 
