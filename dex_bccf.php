c7998e637aa496931eb5df81dadc5c84ef3de823
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-09 09:59:18 +0000

    Fixed IPN address under SSL connections.
    git-svn-id: https://plugins.svn.wordpress.org/booking-calendar-contact-form/trunk@1195334 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_bccf.php
+++ dex_bccf.php
@@ -1379,8 +1379,8 @@ function cp_bccf_get_FULL_site_url($admin = false)
     $pos = strpos($url, "://");
     if ($pos === false)
         $url = 'http://'.$_SERVER["HTTP_HOST"].$url;
-    if (!empty($_SERVER['HTTPS']))     
-        $url = str_replace("http://","https://",$url);        
+//    if (!empty($_SERVER['HTTPS']))     
+//        $url = str_replace("http://","https://",$url);        
     return $url;
 }
 
